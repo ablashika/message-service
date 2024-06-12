57cc0ee64e4e7ff6e78beebf61254128cb9c227d
ablashika <kofoworola203@gmail.com>
2024-06-12 12:01:16 +0000

    chore:user-service-docker

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,8 +3,20 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { EmailService } from 'src/email-service/email-service.service';
 import { AppService } from 'src/app.service';
+import { ClientsModule,Transport  } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NATS_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          url: 'nats://nats:4222',
+      },
+    }
+    ]),
+  ],
   providers: [UserService, EmailService, AppService,  Logger],
   controllers: [UserController]
 })
